1524eb90f4110f4efa27be4396b4dd37a57733d4
npretto <lordkryss@gmail.com>
2018-12-16 16:52:49 +0100

    pathfinding (sligthly wrong)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 
-#include <tesseract/baseapi.h> //this has to stay on top otherwise the console will be full of errors :(
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
+#include <tesseract/baseapi.h>  //this has to stay on top otherwise the console will be full of errors :(
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
 #include <string>
-#include "./src/CalibratedCamera.cpp"
 #include "./src/Arena.cpp"
+#include "./src/CalibratedCamera.cpp"
 #include "./src/PathFinder.cpp"
 
 const String CALIBRATION_PATH = "./data/calibration";
@@ -18,23 +18,31 @@ bool STOP_AT_EVERY_OCR = false;
 float safetyDistance = 5;
 float robotRadius = 20;
 
-int main(int argc, char **argv)
-{
+CalibratedCamera camera;
+Arena arena;
+PathFinder pf;
 
-  CalibratedCamera camera;
+static void onMouse(int event, int x, int y, int, void *) {
+  vector<GraphNode *> p = pf.testClick(x, y);
+  cv::Mat display(arena.topView.rows, arena.topView.cols, CV_8UC3,
+                  Scalar(100, 100, 100));
+  arena.drawMapOn(display);
+  pf.drawMapOn(display);
+  pf.drawPath(display, p);
+  cv::imshow("Arena parsed", display);
 
-  if (calibrate)
-  {
+  cout << "." << endl;
+}
 
+int main(int argc, char **argv) {
+  if (calibrate) {
     camera.calibrateFromFolder(CALIBRATION_PATH);
     camera.saveCalibrationToFile("./calibration.xml");
 
     cout << "\n\n\nCAMERA CALIBRATED FORM FOLDER\n\n\n";
     camera.saveCalibrationToFile();
     cout << "\n\n\n calibration saved to file\n\n\n";
-  }
-  else
-  {
+  } else {
     cout << "\n\n\n loading calibration from file\n\n\n";
 
     camera.loadCalibrationFromFile();
@@ -42,34 +50,38 @@ int main(int argc, char **argv)
     cout << "\n\n\n calibration loaded from file\n\n\n";
   }
 
-  vector<String> fn;                 // std::string in opencv2.4, but cv::String in 3.0
-  cv::glob("./data/map", fn, false); //read file names inside the folder
+  vector<String> fn;  // std::string in opencv2.4, but cv::String in 3.0
+  cv::glob("./data/map", fn, false);  // read file names inside the folder
   cout << "folder letta" << endl;
 
-  for (int i = 0; i < fn.size(); i++)
-  {
+  for (int i = 0; i < fn.size(); i++) {
     auto file = fn.at(i);
     cout << "Undistorting FILE: " << file << endl;
     cv::Mat image = cv::imread(file);
     camera.undistort(image, image);
-    Arena arena;
     arena.parseImage(image);
 
     cv::imshow("Arena", arena.topView);
 
     // cv::Mat display = arena.topView;
-    cv::Mat display(arena.topView.rows, arena.topView.cols, CV_8UC3, Scalar(100, 100, 100));
+    cv::Mat display(arena.topView.rows, arena.topView.cols, CV_8UC3,
+                    Scalar(100, 100, 100));
     arena.drawMapOn(display);
 
     cv::imshow("Arena parsed", display);
 
-    PathFinder pf(arena);
-
+    pf.fromArena(arena);
     pf.drawMapOn(display);
 
     cv::imshow("Arena parsed", display);
 
+    setMouseCallback("Arena parsed", onMouse, 0);
+    // pf.testClick(500, 600);
+
     waitKey(0);
+    for (;;) {
+      waitKey(10);
+    }
   }
 
   waitKey(0);
--- src/PathFinder.cpp
+++ src/PathFinder.cpp
@@ -1,173 +1,238 @@
 
 #pragma once
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
 #include <string>
-#include "./DigitOCR.cpp"
-#include "./Arena.cpp"
-#include "./utils.cpp"
 #include "../main.h"
+#include "./Arena.cpp"
+#include "./DigitOCR.cpp"
+#include "./utils.cpp"
 
 using namespace std;
 using namespace cv;
 
-struct CircumscribedObstacle
-{
-    Point center;
-    float innerRadius;
-    float radius;
+struct CircumscribedObstacle {
+  Point center;
+  float innerRadius;
+  float radius;
 };
 
-struct GraphNode
-{
-    Point center;
-    bool ok;
+struct Edge;
+
+struct GraphNode {
+  Point center;
+  bool ok;
+  vector<Edge *> links;
+  float cost;
+  bool operator<(const GraphNode &rhs) const { return cost > rhs.cost; }
 };
 
-struct Edge
-{
-    GraphNode *a;
-    GraphNode *b;
+float heuristic(GraphNode *start, GraphNode *end) {
+  // return 1;
+  return cv::norm(start->center - end->center);
+}
+
+struct Edge {
+  GraphNode *a;
+  GraphNode *b;
+  float cost;
 };
 
-class PathFinder
-{
-  private:
-    CircumscribedObstacle
-    poligonToCircle(vector<cv::Point> points)
-    {
-        cv::Point2f center(0.0f, 0.0f);
-        for (auto p : points)
-        {
-            center.x += p.x;
-            center.y += p.y;
-        }
-
-        center.x = center.x / points.size();
-        center.y = center.y / points.size();
-
-        float maxRadius = 0;
-        for (Point2f p : points)
-        {
-            float radius = cv::norm(center - p);
-            if (radius > maxRadius)
-                maxRadius = radius;
-        }
-
-        return CircumscribedObstacle{center, maxRadius, maxRadius + robotRadius};
+class PathFinder {
+ private:
+  CircumscribedObstacle poligonToCircle(vector<cv::Point> points) {
+    cv::Point2f center(0.0f, 0.0f);
+    for (auto p : points) {
+      center.x += p.x;
+      center.y += p.y;
     }
 
-    void createNodes(float distance)
-    {
-        int cols = arena.getWidth() / distance;
-        int rows = arena.getHeight() / distance;
-        for (int row = 0; row < rows; row++)
-        {
-            vector<GraphNode> rowVector;
-            for (int col = 0; col < cols; col++)
-            {
-                bool ok = true;
-                Point p(col * distance, row * distance);
-                //force field degli ostacoli
-                Point forceSum(0, 0);
-                for (CircumscribedObstacle o : obstacles)
-                {
-                    Point distancePoint = p - o.center;
-                    float distance = cv::norm(distancePoint);
-                    if (distance > o.radius) //outside the obstacle
-                    {
+    center.x = center.x / points.size();
+    center.y = center.y / points.size();
 
-                        // forceSum += 1 / (distance - o.radius) * distancePoint;
-                    }
-                    else
-                    { //inside the obstacle
-                        if (distance < o.innerRadius)
-                        {
-                            ok = false;
-                            continue;
-                        }
-                        else
-                        {
-                            forceSum = (o.radius - distance + safetyDistance) * distancePoint / cv::norm(distancePoint);
-                        }
-                    }
-                }
+    float maxRadius = 0;
+    for (Point2f p : points) {
+      float radius = cv::norm(center - p);
+      if (radius > maxRadius) maxRadius = radius;
+    }
 
-                Point position = ok ? p + forceSum : p;
-                GraphNode node{position, ok};
-                // rowVector.push_back(node);
-                if (ok)
-                    nodes.push_back(node);
+    return CircumscribedObstacle{center, maxRadius, maxRadius + robotRadius};
+  }
+
+  void createNodes(float distance) {
+    int cols = arena.getWidth() / distance;
+    int rows = arena.getHeight() / distance;
+    for (int row = 0; row < rows; row++) {
+      vector<GraphNode> rowVector;
+      for (int col = 0; col < cols; col++) {
+        bool ok = true;
+        Point p(col * distance, row * distance);
+        // force field degli ostacoli
+        Point forceSum(0, 0);
+        for (CircumscribedObstacle o : obstacles) {
+          Point distancePoint = p - o.center;
+          float distance = cv::norm(distancePoint);
+          if (distance > o.radius) {  // outside the obstacle
+            // forceSum += 1 / (distance - o.radius) * distancePoint;
+          } else {  // inside the obstacle
+            if (distance < o.innerRadius) {
+              ok = false;
+              continue;
+            } else {
+              forceSum = (o.radius - distance + safetyDistance) *
+                         distancePoint / cv::norm(distancePoint);
             }
-            // nodesMap.push_back(rowVector);
+          }
         }
+
+        Point position = ok ? p + forceSum : p;
+        GraphNode node{position, ok};
+        // rowVector.push_back(node);
+        if (ok) nodes.push_back(node);
+      }
+      // nodesMap.push_back(rowVector);
+    }
+  }
+
+  void createEdges(float distance) {
+    for (int i = 0; i < nodes.size(); i++) {
+      for (int j = i; j < nodes.size(); j++) {
+        GraphNode *a = &nodes[i];
+        GraphNode *b = &nodes[j];
+
+        float dist = cv::norm(a->center - b->center);
+
+        if (dist < distance) {
+          Edge *e = new Edge{a, b, heuristic(a, b)};
+          a->links.push_back(e);
+          b->links.push_back(e);
+
+          edges.push_back(e);
+        }
+      }
+    }
+  }
+
+ public:
+  Arena arena;
+  vector<CircumscribedObstacle> obstacles;
+  // vector<vector<GraphNode>> nodesMap;
+  vector<GraphNode> nodes;
+  vector<Edge *> edges;
+
+  void fromArena(Arena a) {
+    arena = a;
+
+    for (auto polygon : arena.obstacles) {
+      obstacles.push_back(poligonToCircle(polygon));
+    }
+    createNodes(30);
+    createEdges(40);
+  }
+
+  void drawMapOn(const cv::Mat &image) {
+    // circles that contains obstacles
+    for (CircumscribedObstacle obstacle : obstacles) {
+      circle(image, obstacle.center, obstacle.innerRadius, Scalar(20, 20, 100),
+             1, LINE_AA);
+      circle(image, obstacle.center, obstacle.radius, Scalar(20, 20, 255), 2,
+             LINE_AA);
     }
 
-    void createEdges(float distance)
-    {
-        for (int i = 0; i < nodes.size(); i++)
-        {
-            for (int j = i; j < nodes.size(); j++)
-            {
-                GraphNode *a = &nodes[i];
-                GraphNode *b = &nodes[j];
-
-                float dist = cv::norm(a->center - b->center);
-
-                if (dist < distance)
-                {
-                    edges.push_back(Edge{a, b});
-                }
-            }
-        }
+    // edges
+    for (Edge *e : edges) {
+      line(image, e->a->center, e->b->center, Scalar(120, 120, 120), 1);
     }
 
-  public:
-    Arena arena;
-    vector<CircumscribedObstacle> obstacles;
-    // vector<vector<GraphNode>> nodesMap;
-    vector<GraphNode> nodes;
-    vector<Edge> edges;
+    // nodes
+    for (GraphNode node : nodes) {
+      if (node.ok)
+        circle(image, node.center, 2, Scalar(255, 255, 255), 1, LINE_AA);
+      else
+        circle(image, node.center, 2, Scalar(0, 0, 255), 1, LINE_AA);
+    }
+  }
 
-    PathFinder(Arena a)
-    {
-        arena = a;
+  // based on
+  // https://www.redblobgames.com/pathfinding/a-star/implementation.html
+  vector<GraphNode *> getPath(GraphNode *start, GraphNode *goal) {
+    vector<GraphNode *> path;
 
-        for (auto polygon : arena.obstacles)
-        {
-            obstacles.push_back(poligonToCircle(polygon));
+    std::unordered_map<GraphNode *, GraphNode *> came_from;
+    std::unordered_map<GraphNode *, float> cost_so_far;
+
+    priority_queue<GraphNode *> frontier;
+
+    start->cost = 0;
+    frontier.push(start);
+
+    came_from[start] = start;
+    cost_so_far[start] = 0;
+
+    while (!frontier.empty()) {
+      GraphNode *current = frontier.top();
+      frontier.pop();
+
+      if (current == goal) {
+        break;
+      }
+      for (Edge *link : current->links) {
+        GraphNode *next = link->a == current ? link->b : link->a;
+
+        float new_cost = cost_so_far[current] + link->cost;
+        if (cost_so_far.find(next) == cost_so_far.end() ||
+            new_cost < cost_so_far[next]) {
+          cost_so_far[next] = new_cost;
+
+          float priority = new_cost + heuristic(next, goal);
+          next->cost = priority;
+          frontier.push(next);
+          came_from[next] = current;
         }
-        createNodes(30);
-        createEdges(40);
+      }
+    }
+    GraphNode *n = goal;
+    path.push_back(goal);
+    while (n != start) {
+      GraphNode *prev = came_from[n];
+      path.push_back(prev);
+      n = prev;
     }
 
-    void drawMapOn(cv::Mat &image)
-    {
-        //circles that contains obstacles
-        for (CircumscribedObstacle
-                 obstacle : obstacles)
-        {
-            circle(image, obstacle.center, obstacle.innerRadius, Scalar(20, 20, 100), 1, LINE_AA);
-            circle(image, obstacle.center, obstacle.radius, Scalar(20, 20, 255), 2, LINE_AA);
-        }
+    return path;
+  }
 
-        //edges
-        for (auto e : edges)
-        {
-            line(image, e.a->center, e.b->center, Scalar(120, 120, 120), 1);
-        }
-
-        //nodes
-        for (GraphNode node : nodes)
-        {
-            if (node.ok)
-                circle(image, node.center, 2, Scalar(255, 255, 255), 1, LINE_AA);
-            else
-                circle(image, node.center, 2, Scalar(0, 0, 255), 1, LINE_AA);
-        }
-
+  void drawPath(Mat image, vector<GraphNode *> path) {
+    for (int i = 0; i < path.size() - 1; i++) {
+      line(image, path[i]->center, path[i + 1]->center, Scalar(120, 120, 255),
+           3);
     }
+  }
+
+  GraphNode *getClosestNode(Point p) {
+    GraphNode *closest;
+    float minDistance = 10000;
+    for (GraphNode &n : nodes) {
+      float dist = cv::norm(n.center - p);
+      if (dist < minDistance) {
+        minDistance = dist;
+        closest = &n;
+      }
+    }
+    cout << closest->center.x << ", " << closest->center.y << endl;
+    return closest;
+  }
+
+  vector<GraphNode *> testClick(int x, int y) {
+    cout << x << "  " << y << endl;
+
+    GraphNode *start = getClosestNode(Point(x, y));
+    GraphNode *end = getClosestNode(Point(200, 300));
+    vector<GraphNode *> path = getPath(start, end);
+
+    return path;
+  }
 };
